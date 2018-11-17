97fc6399563291beab6782b0317ba185c425fdaf
lmjing <lmjing1234@gmail.com>
2018-11-17 17:00:12 +0900

    챕터7 문제 확인

---
+++ src/Book/Chapter7.java
@@ -0,0 +1,33 @@
+package Book;
+
+
+
+public class Chapter7 {
+
+	interface Lendable {
+		enum State {
+			NORMAL, BORROWED;
+		}
+		void checkOut(String borrower, String date);
+		void checkIn();
+	}
+	
+	class SeperateVolume implements Lendable {
+		Lendable.State state;
+		
+		@Override
+		public void checkOut(String borrower, String date) {
+			if (state != Lendable.State.BORROWED) {
+				return;
+			}
+			this.state = Lendable.State.BORROWED;
+		}
+
+		@Override
+		public void checkIn() {
+			this.state = Lendable.State.NORMAL;
+		}
+	}
+
+}
+
