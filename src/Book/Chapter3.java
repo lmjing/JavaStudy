5acb46b8a0739e13ec18ff5e57f1d6f1c2d95f3e
lmjing <lmjing1234@gmail.com>
2018-11-17 12:49:25 +0900

    챕터3 문제 확인

---
+++ src/Book/Chapter3.java
@@ -0,0 +1,17 @@
+package Book;
+
+import java.util.*;
+
+public class Chapter3 {
+	public static void main (String[] args) {
+		System.out.println(" ");
+		System.out.println(0.0);
+		System.out.println("\r\n");
+		System.out.println(.5e100);
+		System.out.println(0xABCFABCDEFL);
+		System.out.println(0x12.5p2);
+		System.out.println("´Ü°¡: \\10000");
+		System.out.println(true);
+		System.out.println(1e-100);
+	}
+}
