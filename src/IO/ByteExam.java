13a6abef4350a2088ad27c2473b05eb6a22fd344
lmjing <lmjing1234@gmail.com>
2018-11-17 12:36:35 +0900

    mac 한글 문제 해결

--- src/IO/ByteExam.java
+++ src/IO/ByteExam.java
@@ -10,14 +10,16 @@ public class ByteExam {
 		
 		try {
 			fis = new FileInputStream("src/IO/ByteExam.java");
-
+			// 앞에 아무것도 안쓰면 프로젝트 밑에 자동 생성
 			fos = new FileOutputStream("byte.txt");
-						
+			
+			// 마지막인 값을 구분하기 위하여
 			int readData = -1;
 			while((readData = fis.read()) != -1) {
 				fos.write(readData);
 			}
 		} catch (Exception e) {
+			// try-catch 자동생성은 FileNotFoundException 이지만 다른 Exception도 받기 위하여 수정
 			e.printStackTrace();
 		} finally {
 			try {
