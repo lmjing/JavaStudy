8f461d2662464b6e74c7f1c661cca24be724678c
lmjing <lmjing1234@gmail.com>
2019-10-20 23:34:25 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println(solution2("1111", "2222"));
+	}
+	
+	static boolean solution(int n) {
+		boolean answer = false;
+		int revertedHalf = 0;
+	      while(n > revertedHalf) {
+	          revertedHalf = revertedHalf * 10 + n % 10;
+	          n /= 10;
+	      }
+	      return n == revertedHalf || n == revertedHalf/10;
+	}
+	
+	static String solution2(String a, String b) {
+		int answer = Integer.parseInt(a) + Integer.parseInt(b);
+		return Integer.toString(answer);
 	}
 }
